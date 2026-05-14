918042c39e4b5428d966b9a886484bd5e11826bd
tactik8 <admin@tactik8.com>
2026-05-14 07:35:09 -0400

    go

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import * as thing from './things.js'
 import * as apiClient from './apiClient.js'
 import * as rdf from './rdfHelpers.js'
 import * as recordIDHelpers from './recordIdHelpers.js'
-import { records as testRecords }  from './records/records.js'
+import { records  }  from './records/records.js'
 
 export * from './jsonldBase.js'
 export * from './dotHelpers.js'
 export * from './things.js'
 export * from './apiClient.js'
 export * from './rdfHelpers.js'
-export {testRecords} from './records/records.js'
+export {records} from './records/records.js'
 
 
-export default { ...helpers, ...thing, ...apiClient, ...rdf, ...recordIDHelpers, testRecords};
+export default { ...helpers, ...thing, ...apiClient, ...rdf, ...recordIDHelpers, records};
 
